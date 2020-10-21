e511c318667d77d210a87062a1f92438ae47e71d
Ada Zhang <73206938+ada-zhang-00@users.noreply.github.com>
2020-10-20 23:08:58 -0400

    Create lab1.py

---
+++ Lab 1 Basic Python/lab1.py
@@ -0,0 +1,49 @@
+from lab1_functions import draw_shape, check_num, check_magnitude
+from math import sqrt
+
+# Task 1: Mathematical operations ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Change the (+) operators to make 'num' equal to 1
+
+# 'num' will store the result of the calculation
+num = (((4 * 4) / 4) / 4)
+check_num(num)
+
+# Task 2: Vector magnitude ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Calculate the magnitude of a 2D vector
+# Note: 'x' and 'y' represent coordinates in a vector < x, y >
+# Hint: sqrt(4) performs a square root on 4 and returns 2
+
+x = 140.15
+y = 144.75
+
+# Replace 1.0 with your final answer
+magnitude = 201.48
+check_magnitude(magnitude)
+
+# Task 3: Drawing shapes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Define a list of coordinates to draw some shapes
+
+# Here's an isosceles triangle as an example
+draw_shape([
+    (-100, 0),
+    (100, 0),
+    (0, 300),
+    (-100, 0)
+])
+
+# 3.1: Equilateral triangle
+draw_shape([
+    (-200,0),
+    (200,0),
+    
+])
+
+# 3.2: Square
+draw_shape([
+    
+])
+
+# 3.3: Any shape with 5+ vertices
+draw_shape([
+    
+])
